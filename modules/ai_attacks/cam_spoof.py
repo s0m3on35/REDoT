e31b85a62144d4c1ade7a0078e1f7e4a15ab4de4
someone <218046406+s0m3on35@users.noreply.github.com>
2025-06-27 11:32:53 +0200

    Update cam_spoof.py

--- modules/ai_attacks/cam_spoof.py
+++ modules/ai_attacks/cam_spoof.py
@@ -1,6 +1,6 @@
 # AI Camera Spoofing Module
 import cv2
-print("📸 Launching camera spoofing module...")
+print(" Launching camera spoofing module...")
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot access camera.")
@@ -9,7 +9,7 @@ else:
         ret, frame = cap.read()
         if not ret:
             break
-        cv2.putText(frame, "👁️ Adversarial Spoof Active", (10, 30),
+        cv2.putText(frame, " Adversarial Spoof Active", (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
         cv2.imshow("RedOT Cam Feed", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
